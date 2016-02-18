64cdd4a13b5ebd5aeadbbbe17304d6d1cad624fc
Matt Nadareski <mnadareski@gmail.com>
2016-02-17 17:26:10 -0800

    Update SQL script to create bare database

--- wizzardRedux/scripts/init.sql
+++ wizzardRedux/scripts/init.sql
@@ -1,91 +1,172 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2016 at 02:24 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `roms`
+-- Database: `wod`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table Structure for Table `checksums`
+-- Table structure for table `checksums`
 --
 
-CREATE TABLE IF NOT EXISTS `checksums` (
+CREATE TABLE `checksums` (
   `file` int(11) NOT NULL,
-  `size` bigint(12) NOT NULL,
-  `crc` char(8) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `md5` char(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `sha1` char(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`file`,`size`,`crc`,`md5`,`sha1`),
-  KEY `file` (`file`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=DYNAMIC;
+  `size` int(11) NOT NULL,
+  `crc` varchar(8) NOT NULL,
+  `md5` varchar(32) NOT NULL,
+  `sha1` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table Structure for Table `files`
+-- Table structure for table `files`
 --
 
-CREATE TABLE IF NOT EXISTS `files` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `set` int(11) NOT NULL,
-  `name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `type` char(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `set` (`set`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=DYNAMIC AUTO_INCREMENT=49781487 ;
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `setid` int(11) NOT NULL,
+  `name` varchar(1024) NOT NULL,
+  `type` varchar(1024) NOT NULL DEFAULT 'rom'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table Structure for Table `games`
+-- Table structure for table `games`
 --
 
-CREATE TABLE IF NOT EXISTS `games` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
   `system` int(11) NOT NULL,
-  `name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `source` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `system` (`system`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=DYNAMIC AUTO_INCREMENT=22416052 ;
+  `name` varchar(1024) NOT NULL,
+  `source` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table Structure for Table `systems`
+-- Table structure for table `sources`
 --
 
-CREATE TABLE IF NOT EXISTS `systems` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `manufactor` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `system` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT AUTO_INCREMENT=480 ;
+CREATE TABLE `sources` (
+  `id` int(11) NOT NULL,
+  `name` varchar(767) NOT NULL,
+  `lastupdated` datetime NOT NULL,
+  `url` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Constraints der exportierten Tabellen
+-- Table structure for table `systems`
+--
+
+CREATE TABLE `systems` (
+  `id` int(11) NOT NULL,
+  `manufacturer` varchar(1024) NOT NULL,
+  `system` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Constraints der Tabelle `checksums`
+-- Indexes for table `checksums`
 --
 ALTER TABLE `checksums`
-  ADD CONSTRAINT `checksums_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `systems` (`id`) ON DELETE CASCADE;
+  ADD PRIMARY KEY (`file`,`size`,`crc`,`md5`,`sha1`);
 
 --
--- Constraints der Tabelle `files`
+-- Indexes for table `files`
 --
 ALTER TABLE `files`
-  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `games` (`id`) ON DELETE CASCADE;
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `FK_games_files_Cascade` (`setid`);
 
 --
--- Constraints der Tabelle `games`
+-- Indexes for table `games`
 --
 ALTER TABLE `games`
-  ADD CONSTRAINT `games_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `datfiles` (`id`) ON DELETE CASCADE;
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `sources`
+--
+ALTER TABLE `sources`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `name_2` (`name`),
+  ADD KEY `name_3` (`name`);
+
+--
+-- Indexes for table `systems`
+--
+ALTER TABLE `systems`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sources`
+--
+ALTER TABLE `sources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=207;
+--
+-- AUTO_INCREMENT for table `systems`
+--
+ALTER TABLE `systems`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=819;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `checksums`
+--
+ALTER TABLE `checksums`
+  ADD CONSTRAINT `FK_files_checksums_Cascade` FOREIGN KEY (`file`) REFERENCES `files` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `FK_games_files_Cascade` FOREIGN KEY (`setid`) REFERENCES `files` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
