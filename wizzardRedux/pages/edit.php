41e5ed744910792c7d6ddb50c606577fef38355d
Matt Nadareski <mnadareski@gmail.com>
2016-02-18 02:23:45 -0800

    Added some initial logic and notes to edit.php

--- wizzardRedux/pages/edit.php
+++ wizzardRedux/pages/edit.php
@@ -3,9 +3,19 @@ Add, edit, and remove data from the database manually.
 
 Requires:
 	type	The type of data to work with [system, source, game]
+	
+NOTE: Is it necessary to have a switch on the type? Can't everything be on the same page?
+
+Flow: Choose System / Source from auto-populated dropdowns -> Game dropdown appears, filtered
+Persistent have Add System and Add Source buttons
+Conditional Edit System, Remove System, Edit Source, Remove Source if a valid (non-0) source is selected
+Persistent Add Game button
+Conditional Edit Game, Remove Game buttons if a valid (non-0) source is selected
 -->
 
 <?php
+
+$path_to_root = (getcwd() == "/wod/" ? "" : "..");
 
 if (!isset($_GET["type"]))
 {
@@ -24,5 +34,12 @@ if ($type != "system" && $type != "source" && $type != "game")
 }
 
 echo "Valid type detected: ".$type."<br/>";
+
+switch ($type)
+{
+	case "system": break;
+	case "source": break;
+	case "games": break;
+}
 
 ?>
