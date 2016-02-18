e7367f060c61ca18c80a8dcbb5fa8fa379d9196a
Matt Nadareski <mnadareski@gmail.com>
2016-02-18 00:04:50 -0800

    404! 503!

---
+++ wizzardRedux/pages/404.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<h1>Really? Your're at the 404?</h1>
+<p>That\'s great. Really really great. I hope you\'re proud of yourself.
+		Why don\'t you press the button below and give that another shot...</p>";
+
+?>
