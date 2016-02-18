ea51f14d4a0837456c0cbf9d675ac457d81412df
Matt Nadareski <mnadareski@gmail.com>
2016-02-18 00:07:39 -0800

    Cry for help added

--- wizzardRedux/css/style.php
+++ wizzardRedux/css/style.php
@@ -1,3 +1,11 @@
+<!-- 
+Any specialized inline styles or code that isn't really a helper goes here.
+
+Note: The original programmer (mnadareski/darksabre76) speaking: I don't have an artistic
+	bone in my body, so if anyone knows their way around making a site pretty, please contact
+	me. That is unless vibrant primary colors and bad <div> usage is your groove.
+-->
+
 <?php
 
 $tab = "&nbsp;&nbsp;&nbsp;&nbsp;";
