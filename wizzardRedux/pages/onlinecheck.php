e4eafd819c0a7f9b0c17dd0362929687f8c8b2e7
Matt Nadareski <mnadareski@gmail.com>
2016-02-19 00:53:24 -0800

    Add onlinecheck.php initial logic

--- wizzardRedux/pages/onlinecheck.php
+++ wizzardRedux/pages/onlinecheck.php
@@ -3,16 +3,45 @@
 /*
 Check for new downloadable ROMs from all available sites
 
+Requires:
+	source		The sourcename to check against
+
 Note: This is the most tedious one of all. All of the checks should be named as "sites/<sitename>.php".
 
 If the page is sent with no param, generate a list of all possible checks. Each check has a
-<sitename>-id.txt file attached to it that designates what files have already been found. Use
+<sitename>.txt file next to it that designates what files have already been found. Use
 the existing <sitename>/onlinecheck.php files for either reference or wholesale repurposing.
 
-TODO: Figure out where the id files should be stored (temp/ids/?, sites/ids/?)
 TODO: Retool existing onlinecheck.php files to follow the new format
 TODO: Add a way to figure out if a site is dead based on the original list that WoD created
 TODO: Figure out more todos
 */
+
+if (!isset($_GET["source"]))
+{
+	// List all files, auto-generate links to proper pages
+	$files = scandir("sites/");
+	foreach ($files as $file)
+	{
+		if (preg_match("/^.*\.php$/", $file))
+		{
+			echo "<a href=\"?page=onlinecheck&source=".chop($file, ".php")."&debug=1\">".htmlspecialchars($file)."</a><br/>";
+		}
+	}
+
+	echo "<br/><a href='".$path_to_root."/index.php'>Return to home</a>";
+
+	die();
+}
+elseif (!file_exists("sites/".$_GET["source"].".php"))
+{
+	echo "<b>The file you supply must be in /wod/sites/</b><br/>";
+	echo "<a href='index.php'>Return to home</a>";
+
+	die();
+}
+
+// If we get to this point, we assume that it's good
+include_once("sites/".$_GET["source"].".php");
 
 ?>
