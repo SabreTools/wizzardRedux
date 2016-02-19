b302874788dfd8460912c408672ae0b0bd6bce86
Matt Nadareski <mnadareski@gmail.com>
2016-02-18 19:07:59 -0800

    Create blank onlinecheck.php master page. Added relevant notes.

---
+++ wizzardRedux/pages/onlinecheck.php
@@ -0,0 +1,18 @@
+<!--
+Check for new downloadable ROMs from all available sites
+
+Note: This is the most tedious one of all. All of the checks should be named as "sites/<sitename>.php".
+
+If the page is sent with no param, generate a list of all possible checks. Each check has a
+<sitename>-id.txt file attached to it that designates what files have already been found. Use
+the existing <sitename>/onlinecheck.php files for either reference or wholesale repurposing.
+
+TODO: Figure out where the id files should be stored (temp/ids/?, sites/ids/?)
+TODO: Retool existing onlinecheck.php files to follow the new format
+TODO: Add a way to figure out if a site is dead based on the original list that WoD created
+TODO: Figure out more todos
+-->
+
+<?php
+
+?>
