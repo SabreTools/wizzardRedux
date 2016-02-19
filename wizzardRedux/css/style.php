f6eebca98ade08d7ef343ef60d637c26a479685c
Matt Nadareski <mnadareski@gmail.com>
2016-02-18 21:51:26 -0800

    [doc] missed one

--- wizzardRedux/css/style.php
+++ wizzardRedux/css/style.php
@@ -1,12 +1,12 @@
-<!-- 
+<?php
+
+/*
 Any specialized inline styles or code that isn't really a helper goes here.
 
 Note: The original programmer (mnadareski/darksabre76) speaking: I don't have an artistic
-	bone in my body, so if anyone knows their way around making a site pretty, please contact
-	me. That is unless vibrant primary colors and bad <div> usage is your groove.
--->
-
-<?php
+bone in my body, so if anyone knows their way around making a site pretty, please contact
+me. That is unless vibrant primary colors and bad <div> usage is your groove.
+*/
 
 $tab = "&nbsp;&nbsp;&nbsp;&nbsp;";
 
