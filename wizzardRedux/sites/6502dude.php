67549df60fdd6d8d5293574c2cee0d4a28c663c3
Matt Nadareski <mnadareski@gmail.com>
2016-02-19 01:50:34 -0800

    First working onlinecheck!

---
+++ wizzardRedux/sites/6502dude.php
@@ -0,0 +1,109 @@
+<?php
+
+echo "<h2>Loading pages and links...</h2>";
+
+$r_query = file("sites/".$_GET["source"].".txt");
+$r_query = array_flip($r_query);
+
+// There is the case that all keys will contain a whitespace character at the end
+$s_query = Array();
+while (list($k, $v) = each($r_query))
+{
+	$s_query[trim($k)] = $r_query[$k];
+}
+$r_query = $s_query;
+unset($s_query);
+
+$base_dl_url="http://armas.cbm8bit.com/tapescans/victaps/";
+
+$pages=Array(
+		'http://armas.cbm8bit.com/0to9taps.html',
+		'http://armas.cbm8bit.com/ataps.html',
+		'http://armas.cbm8bit.com/btaps.html',
+		'http://armas.cbm8bit.com/ctaps.html',
+		'http://armas.cbm8bit.com/dtaps.html',
+		'http://armas.cbm8bit.com/etaps.html',
+		'http://armas.cbm8bit.com/ftaps.html',
+		'http://armas.cbm8bit.com/gtaps.html',
+		'http://armas.cbm8bit.com/htaps.html',
+		'http://armas.cbm8bit.com/itaps.html',
+		'http://armas.cbm8bit.com/jtaps.html',
+		'http://armas.cbm8bit.com/ktaps.html',
+		'http://armas.cbm8bit.com/ltaps.html',
+		'http://armas.cbm8bit.com/mtaps.html',
+		'http://armas.cbm8bit.com/ntaps.html',
+		'http://armas.cbm8bit.com/otaps.html',
+		'http://armas.cbm8bit.com/ptaps.html',
+		'http://armas.cbm8bit.com/qtaps.html',
+		'http://armas.cbm8bit.com/rtaps.html',
+		'http://armas.cbm8bit.com/staps.html',
+		'http://armas.cbm8bit.com/ttaps.html',
+		'http://armas.cbm8bit.com/utaps.html',
+		'http://armas.cbm8bit.com/vtaps.html',
+		'http://armas.cbm8bit.com/wtaps.html',
+		'http://armas.cbm8bit.com/xtaps.html',
+		'http://armas.cbm8bit.com/ytaps.html',
+		'http://armas.cbm8bit.com/ztaps.html',
+		);
+
+$found = Array();
+
+foreach($pages as $newfile)
+{
+	$query=implode ('', file ($newfile));
+	$query=preg_replace('/(\s+)/',' ', $query); // Remove all whitespace
+	$query=preg_replace('/(href=)("?)(\S+?)("?)(>)/','\1"\3"\5', $query); // Make sure all hrefs are quoted properly
+	$query=explode ('<tr ',$query);
+	$query[0]=null;
+
+	$old=0;
+	$new=0;
+
+	foreach($query as $row)
+	{
+		if($row)
+		{
+			$row=explode('<td', $row);
+			$title=trim(str_replace(" ( NO SCAN YET )", "", strip_tags('<td'.$row[1])));
+			$info=trim(strip_tags('<td'.$row[2]));
+			$dls=explode ('tapescans/victaps/', $row[3]);
+			$dls[0]=null;
+
+			foreach($dls as $dl)
+			{
+				if($dl)
+				{
+					$dl=explode ('"', $dl);
+					$dl=$dl[0];
+						
+					$ext=explode ('.', $dl);
+					$ext=$ext[count($ext)-1];
+
+					if($r_query[$dl])
+					{
+						$old++;
+					}
+					else
+					{
+						$found[]=Array($dl,$title." (".$info.").".$ext);
+						$new++;
+					}
+				}
+			}
+		}
+	}
+
+	echo "Loading ".$newfile.$tab;
+	echo "Found new: ".$new.", old: ".$old."<br/>";
+}
+
+echo "<h2>New files:</h2>";
+
+foreach($found as $row)
+{
+	echo "<a href='".$base_dl_url.$row[0]."'>".$row[0]."</a><br/>";
+}
+
+echo "\n";
+
+?>
