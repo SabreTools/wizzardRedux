48d199cdc6d1ae059e010166a42a288ea50901e8
Matt Nadareski <mnadareski@gmail.com>
2016-03-08 09:39:21 -0800

    Add an end when the folder is empty

--- wizzardRedux/admin/import.php
+++ wizzardRedux/admin/import.php
@@ -85,6 +85,13 @@ if (!isset($_GET["filename"]))
 			}
 		}
 	}
+	elseif (!isset($_GET["rt"]))
+	{
+		echo "<script type='text/javascript'>window.location='?page=import&rt=1".
+				($size ? "&size=1" : "").
+				($type != "" ? "&type=".$type : "").
+				"'</script>";
+	}
 }
 else
 {
