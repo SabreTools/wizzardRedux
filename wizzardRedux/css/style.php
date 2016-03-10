832d7dc17c7ca38ac22571a6b0f8ac2a3e0de573
Matt Nadareski <mnadareski@gmail.com>
2016-03-10 01:07:09 -0800

    Update naming convention
    I realized that I referenced myself disparately across multiple places.
    In order to make things as airtight as possible, I'm using both my real
    name and internet name for recognition's sake. This is NOT required of
    all contributors (especially since I have no idea WoD's real name).

--- wizzardRedux/css/style.php
+++ wizzardRedux/css/style.php
@@ -5,7 +5,7 @@ Any specialized inline styles or code that isn't really a helper goes here.
 
 Original code: The Wizard of DATz
 
-Note: The original programmer (mnadareski/darksabre76) speaking: I don't have an artistic
+Note: Matt Nadareski (darksabre76) speaking: I don't have an artistic
 bone in my body, so if anyone knows their way around making a site pretty, please contact
 me. That is unless vibrant primary colors and bad <div> usage is your groove.
 */
