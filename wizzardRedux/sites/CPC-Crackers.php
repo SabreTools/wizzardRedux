e50aeee8ab9289934d03b8c90480dc1ba558a962
Matt Nadareski <mnadareski@gmail.com>
2016-03-16 01:14:32 -0700

    Clean up CPC-Crackers online checker

--- wizzardRedux/sites/CPC-Crackers.php
+++ wizzardRedux/sites/CPC-Crackers.php
@@ -7,38 +7,36 @@ $dirs = array(
 	'Downloads/',
 );
 
-print "<pre>check folders:\n\n";
-
+echo "<table>\n";
 foreach ($dirs as $dir)
 {
-	print "load: ".$dir."\n";
+	echo "<tr><td colspan=2><b>".$dir."</b></td></tr>";
 	$query = get_data("http://cpccrackers.free.fr/".$dir);
-	$query = explode('</h1>', $query);
-	$query = explode('href="index.php?voirdir=', $query[1]);
-	array_splice($query, 0, 1);
+	
+	preg_match_all("/<a rel=\"nofollow\" href=\"index\.php\?voirdir=(.*?)\" class=\".*?\">.*?<\/a>/", $query, $pages);
+	$pages = $pages[1];
 
-	foreach ($query as $row)
+	foreach ($pages as $row)
 	{
-		$row = explode('"', $row);
-		$row = $row[0];
-		
 		$new = 0;
 		$old = 0;
 
-		print "load: ".$row."\n";
+		echo "<tr><td>".$row."</td>";
 		$bquery = get_data("http://cpccrackers.free.fr/".$dir."/index.php?voirdir=".$row);
-		$bquery = explode('<td class="g2c" width="120px">', $bquery);
-		$bquery = explode('href="', $bquery[1]);
-		array_splice($bquery, 0, 1);
-
-		foreach ($bquery as $brow)
+		
+		preg_match_all("/<tr class=\"pair\"><td><a rel=\"nofollow\" href=\"(.*?)\" title=\".*?\">(?:<img.*?>)?(.*?)<\/a><\/td><td align=\"right\" class=\"ctp\"/", $bquery, $urls);
+		
+		$newurls = array();
+		for ($index = 0; $index < sizeof($urls[0]); $index++)
 		{
-			$brow = explode('"', $brow);
-			$url = $brow[0];
-			
-			if (!$r_query[urldecode($dir.$url)])
+			$newurls[] = array($urls[1][$index], $urls[2][$index]);
+		}
+
+		foreach ($newurls as $url)
+		{
+			if (!$r_query[urldecode($dir.$url[0])])
 			{
-				$found[] = $dir.$url;
+				$found[] = array($url[1], "http://cpccrackers.free.fr/".$dir.$url[0]);
 				$new++;
 			}
 			else
@@ -46,19 +44,22 @@ foreach ($dirs as $dir)
 				$old++;
 			}
 		}
-
-		print "close: ".$row."\n";
-		print "new: ".$new.", old: ".$old."\n";
+		
+		echo "<td>Found new: ".$new.", old: ".$old."</tr>\n";
 	}
 }
+echo "</table>\n";
 
-print "\nnew urls:\n\n<table><tr><td><pre>";
-
-foreach ($found as $url)
+if (sizeof($found) > 0)
 {
-	print "<a href=\"http://cpccrackers.free.fr/".$url."\">".urldecode($url)."</a>\n";
+	echo "<h2>New files:</h2>";
 }
 
-print "</td></tr></table>";
+foreach ($found as $row)
+{
+	echo "<a href='".$row[1]."'>".$row[0]."</a><br/>\n";
+}
+
+echo "<br/>\n";
 
 ?>
