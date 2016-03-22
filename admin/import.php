b90a2f4bc11393a9b07899520de8d6b51bfb1cef
Matt Nadareski <mnadareski@gmail.com>
2016-03-22 13:45:35 -0700

    Change strtoupper to strtolower. Upper causes issues with RV

--- admin/import.php
+++ admin/import.php
@@ -522,9 +522,9 @@ function add_rom_helper ($machinename, $romtype, $gameid, $name, $date, $size, $
 					")
 					VALUES (".$romid.
 						($size != "" ? ", ".$size : "").
-						($crc != "" ? ", '".strtoupper($crc)."'" : "").
-						($md5 != "" ? ", '".strtoupper($md5)."'" : "").
-						($sha1 != "" ? ", '".strtoupper($sha1)."'" : "").
+						($crc != "" ? ", '".strtolower($crc)."'" : "").
+						($md5 != "" ? ", '".strtolower($md5)."'" : "").
+						($sha1 != "" ? ", '".strtolower($sha1)."'" : "").
 					")";
 			
 			$result = mysqli_query($link, $query);
