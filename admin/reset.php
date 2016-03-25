9c094d40a72febe07589a422fe64fecd332e4f49
Matt Nadareski <mnadareski@gmail.com>
2016-03-25 16:17:53 -0700

    Remove "are you sure" from reset.
    There's no link to this page. If you get here, it's of your own
    volition. You must want to wipe it.

--- admin/reset.php
+++ admin/reset.php
@@ -17,20 +17,15 @@ ALTER TABLE games AUTO_INCREMENT = 1;
 DELETE FROM parent;
 ALTER TABLE parent AUTO_INCREMENT = 1;";
 
-echo "<a href=\"?page=reset&confirm=1\">Are you sure you want to delete the data?/a>";
+$result = mysqli_query($link, $query);
 
-if (isset($_GET["confirm"]) && $_GET["confirm"] == 1)
+if (gettype($result) == "boolean" && $result)
 {
-	$result = mysqli_query($link, $query);
-	
-	if (gettype($result) == "boolean" && $result)
-	{
-		echo "Resetting database succeeded!<br/>\n";
-	}
-	else
-	{
-		echo "Resetting database failed!<br/>\n".mysqli_error($link);
-	}
+	echo "Resetting database succeeded!<br/>\n";
+}
+else
+{
+	echo "Resetting database failed!<br/>\n".mysqli_error($link);
 }
 
 ?>
