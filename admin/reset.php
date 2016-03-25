49e00cba943fdd7e1089c2aa27667fa059a979b7
Matt Nadareski <mnadareski@gmail.com>
2016-03-25 16:20:29 -0700

    Separate queries just in case.

--- admin/reset.php
+++ admin/reset.php
@@ -5,21 +5,29 @@
  Original code by Matt Nadareski (darksabre76)
  ------------------------------------------------------------------------------------ */
 
+$allreset = true;
+
 $query = "DELETE FROM checksums;
-ALTER TABLE checksums AUTO_INCREMENT = 1;
+ALTER TABLE checksums AUTO_INCREMENT = 1";
 
-DELETE FROM files;
-ALTER TABLE files AUTO_INCREMENT = 1;
+$allreset = $allreset && mysqli_query($link, $query);
 
-DELETE FROM games;
-ALTER TABLE games AUTO_INCREMENT = 1;
+$query = "DELETE FROM files;
+ALTER TABLE files AUTO_INCREMENT = 1";
 
-DELETE FROM parent;
-ALTER TABLE parent AUTO_INCREMENT = 1;";
+$allreset = $allreset && mysqli_query($link, $query);
 
-$result = mysqli_query($link, $query);
+$query = "DELETE FROM games;
+ALTER TABLE games AUTO_INCREMENT = 1";
 
-if (gettype($result) == "boolean" && $result)
+$allreset = $allreset && mysqli_query($link, $query);
+
+$query = "DELETE FROM parent;
+ALTER TABLE parent AUTO_INCREMENT = 1";
+
+$allreset = $allreset && mysqli_query($link, $query);
+
+if ($allreset)
 {
 	echo "Resetting database succeeded!<br/>\n";
 }
