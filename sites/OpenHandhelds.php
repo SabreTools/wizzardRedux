139379d9a304cd869bca8511fa6897f919cf4cd7
Matt Nadareski <mnadareski@outlook.com>
2016-03-30 17:51:16 -0700

    Add skeleton of new checker suggested by jpantarotto

---
+++ sites/OpenHandhelds.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+TODO: Write checker for this site: http://www.openhandhelds.org/index.php?
+TODO: Add source to the sources table (web/desktop)
+TODO: Add new systems for ones not already included (All except Pandora, GP32?)
+TODO: Add read/write from associated txt file
+TODO: Study page layout to make sure that all files are found and catalogued.
+TODO: Be careful of how the URLs are made (mostly 0,0,0,0 etc)
+ */
+
+?>
