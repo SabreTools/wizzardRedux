98a07e0048593aef3108e9387dff22f05951ce52
Matt Nadareski <mnadareski@outlook.com>
2016-04-01 18:20:16 -0700

    Update base test page
    There is some commented code left in for now since RV2XML conversion is
    still up in the air.

--- pages/test.php
+++ pages/test.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "<p>This page is a test...</p>";
+echo "<p>This page is a test...</p><p/>\n";
+
+/*
+$file = file("temp/import/nointro/Atari - 5200 (20130303-220600_CM).dat");
+$file = rv2xml($file);
+
+echo $file;
+var_dump($file);
+*/
 
 ?>
