c4302f0a5d69a5416bd60c5176e197846e94ff38
Matt Nadareski <mnadareski@outlook.com>
2016-04-04 03:19:14 -0700

    Remove test code; verified working

--- pages/test.php
+++ pages/test.php
@@ -2,12 +2,4 @@
 
 echo "<p>This page is a test...</p><p/>\n";
 
-/*
-$file = file("temp/import/nointro/Atari - 5200 (20130303-220600_CM).dat");
-$file = rv2xml($file);
-
-echo $file;
-var_dump($file);
-*/
-
 ?>
